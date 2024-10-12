46db5d2c5d723319c3fec84885e461fc241eb5bf
dhruv4023 <dhruv20345@gmail.com>
2024-10-13 01:25:29 +0530

    k8s microservice done upto auth and main

--- ManageAllMicroService/src/config/config.js
+++ ManageAllMicroService/src/config/config.js
@@ -18,13 +18,13 @@ export default {
     },
     port: process.env.APP_PORT || 5000,
     protocol: process.env.PROTOCOL || 'http',
-    app_base_url: process.env.APP_BASE_URL || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || localhost}:5000/api/v1`,
-    app_project_path: process.env.APP_PROJECT_PATH || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || localhost}:5000`,
+    app_base_url: process.env.APP_BASE_URL || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || "localhost"}:5000/api/v1`,
+    app_project_path: process.env.APP_PROJECT_PATH || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || "localhost"}:5000`,
     node_env: process.env.NODE_ENV || 'development',
     jwt_secret: process.env.JWT_SECRET || "123jwttest",
     micro_services: {
-        auth_api_end: process.env.AUTH_API_END || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || localhost}:5001`,
-        post_api_end: process.env.POST_API_END || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || localhost}:5002`,
-        comments_api_end: process.env.COMMENTS_API_END || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || localhost}:5003`,
+        auth_api_end: process.env.AUTH_API_END || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || "localhost"}:5001`,
+        post_api_end: process.env.POST_API_END || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || "localhost"}:5002`,
+        comments_api_end: process.env.COMMENTS_API_END || `${process.env.PROTOCOL || "http"}://${process.env.APP_HOST || "localhost"}:5003`,
     }
 };
